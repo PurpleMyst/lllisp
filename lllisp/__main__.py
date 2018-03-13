c3411ba43647ebdcb205a21327e4b4249232ea8d
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-03-13 01:34:01 +0100

    Add __main__ argument

--- lllisp/__main__.py
+++ lllisp/__main__.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+import argparse
+
 from .parser import parse
 from .compiler import compile_to_ir
 
 
 def main():
-    with open("example.cl") as f:
+    argp = argparse.ArgumentParser("lllisp")
+    argp.add_argument("filename", type=argparse.FileType("r"),
+                      help="The file to compile.")
+    argv = argp.parse_args()
+
+    with open(argv.filename) as f:
         sexpr = parse(f.read())
 
     ir = compile_to_ir(sexpr)
